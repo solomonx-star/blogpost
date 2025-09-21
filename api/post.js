213e24cc370300ca42e2836e0e6f89e57f887f8e
Solomon kanu <105060839+solomonx-star@users.noreply.github.com>
2025-09-21 16:43:41 +0000

    Refactor post API functions: streamline error handling and improve code consistency

--- api/post.js
+++ api/post.js
@@ -1,39 +1,54 @@
 import apiClient from "./apiClient";
 
-
 export const fetchPosts = async () => {
-    try {
-        const response = await apiClient.get('/all-posts');
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
-}
-
+  try {
+    const response = await apiClient.get("/all-posts");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const fetchPostById = async (id) => {
-    try {
-        const response = await apiClient.get(`/${id}`); 
-        return response.data;
-    } catch (error) {
-        throw error;
-    }   
-}
+  try {
+    const response = await apiClient.get(`/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const createPost = async (postData) => {
-    try {
-        const response = await apiClient.post('/blog-post', postData);   
-        return response.data;
-    } catch (error) {
-        throw error;
-    }   
-}
+export const createPost = async ({ title, content }) => {
+  try {
+    const postdata = {
+      title,
+      content,
+    };
+    const response = await apiClient.post("/blog-post", postdata);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const updatePost = async (id, postData) => {
-    try {
-        const response = await apiClient.put(`/${id}`, postData); 
-        return response.data;
-    } catch (error) {
-        throw error;
-    }   
-}
+export const updatePost = async (id, { title, content }) => {
+  try {
+    const postData = {
+      title,
+      content,
+    };
+    const response = await apiClient.put(`/update/${id}`, postData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deletePost = async (id) => {
+  try {
+    const response = await apiClient.delete(`/delete/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
